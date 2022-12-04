0fa3b5f1d4ebe61be92e3eda79421b309167055d
dattsu <eamsradiohe@gmail.com>
2022-12-05 00:14:29 +0900

    change 依存関係をもつサービスのテストにおいて、Spyによるモックを利用するように修正

--- test/testing-services/demo/src/service/master.service.spec.ts
+++ test/testing-services/demo/src/service/master.service.spec.ts
@@ -2,38 +2,41 @@ import { TestBed } from '@angular/core/testing';
 import { MasterService } from './master.service';
 import { ValueService } from './value.service';
 
+let masterService: MasterService;
+let valueServiceSpy: jasmine.SpyObj<ValueService>;
 
-describe('MasterService without Anuglar tesing support', () => {
-  let masterService: MasterService;
-  let check;
+describe('MasterService', () => {
+  beforeEach(()=>
+  {
+    const spy = jasmine.createSpyObj('ValueService',['getValue']);
 
-it('#getValue should return real value from the real service', ()=>
-{
-  masterService = new MasterService(new ValueService());
-  expect(masterService.getValue()).toBe('real value');
-});
+    TestBed.configureTestingModule({
+      //テスト用サービスとスパイオブジェクトを両方準備する
+      providers: [
+        MasterService,
+        { provide: ValueService, useValue: spy }
+      ]
+    });
+  //サービスとスパイオブジェクトを注入する
+  masterService = TestBed.inject(MasterService);
+  valueServiceSpy = TestBed.inject(ValueService) as jasmine.SpyObj<ValueService>;});
 
-it('#getValue should return stubbed value from a spy', ()=>
-{
-   // ValueServiceを表すオブジェクトに対して get value スパイを作成する
-   const valueServiceSpy = jasmine.createSpyObj('ValueService', ['getValue']);
+  it('#getValue should return stubbed value from a spy', ()=>
+  {
 
-   //get value　スパイが呼ばれたときに、値をセットする
-   const stubValue = 'stub value';
-   valueServiceSpy.getValue.and.returnValue(stubValue);
+     //get value　スパイが呼ばれたときに、値をセットする
+     const stubValue = 'stub value';
+     valueServiceSpy.getValue.and.returnValue(stubValue);
 
-   masterService = new MasterService(valueServiceSpy);
+     expect(masterService.getValue())
+     .withContext('service returned stub value')
+     .toBe(stubValue);
+     expect(valueServiceSpy.getValue.calls.count())
+     .withContext('spy method was called once')
+     .toBe(1);
 
-   expect(masterService.getValue())
-   .withContext('service returned stub value')
-   .toBe(stubValue);
-   expect(valueServiceSpy.getValue.calls.count())
-   .withContext('spy method was called once')
-   .toBe(1);
-
-   expect(valueServiceSpy.getValue.calls.mostRecent().returnValue)
-  .toBe(stubValue);
- });
-
+     expect(valueServiceSpy.getValue.calls.mostRecent().returnValue)
+    .toBe(stubValue);
+   });
 
 });
