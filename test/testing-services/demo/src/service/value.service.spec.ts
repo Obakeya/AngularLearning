638150e362bcfe233bdbff6667d34d5e00a4b37a
dattsu <eamsradiohe@gmail.com>
2022-12-04 23:58:43 +0900

    change TestBedを利用してテストしたいサービスを生成するように修正
    descrbeでくるまないと、コンポーネントが生成される前にコンポーネントのテストが行われる
    https://stackoverflow.com/questions/61957083/what-causes-cannot-configure-the-test-module-when-the-test-module-has-already-b

--- test/testing-services/demo/src/service/value.service.spec.ts
+++ test/testing-services/demo/src/service/value.service.spec.ts
@@ -1,36 +1,18 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed} from '@angular/core/testing';
 import { ValueService } from './value.service';
 
-describe('ValueService', () => {
   let service: ValueService;
 
+  //describeを書かないと、コンポーネントのテスト部分でエラーが発生する
+  describe('ValueService', () => {
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = new ValueService();
+    TestBed.configureTestingModule({providers: [ValueService]
+    });
+    service = TestBed.inject(ValueService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+
+  it('should use ValueService', () => {
+     expect(service.getValue()).toBe('real value');
   });
-
-  it('#getvalue should return real value', () => {
-    expect(service.getValue()).toBe('real value');
   });
-
-  it('getObservableValue should return value from observalbe' ,
-     (done: DoneFn) =>{
-      service.getObservableValue().subscribe(value => {
-        expect(value).toBe('observable value');
-        done();
-      });
-     });
-
-     it('#getPromiseValue should return value from a promise',
-     (done: DoneFn) => {
-     service.getPromiseValue().then(value => {
-       expect(value).toBe('promise value');
-       done();
-     });
-   });
-});
